0473f56e54dc945701aebf099474cabd19ede677
Florian Wiedenbein <florian1940@live.de>
2021-02-15 11:27:06 +0100

    Modded Cars als Enums hinzugefügt

--- ProjectHome_001/Enums.cs
+++ ProjectHome_001/Enums.cs
@@ -6,6 +6,24 @@ namespace ProjectHome_001
 {
     class Enums
     {
+        public enum ModCars : uint
+        {
+            meteor = 1670383606,
+            argento = 1980574343,
+            mf1 = 515525977,
+            alamo = 3840336353,
+            bertholf = 2228523432,
+            VTGAUNTLETSHER = 2250759368,
+            prevolter = 2149939648,
+            hachurar = 1217691477,
+            streiter2 = 3827691514,
+            stew_arias = 3266605137,
+            spvigeronew = 833668677,
+            zr380p = 643711017,
+            firetruk2 = 1706861089,
+        }
+
+
         public enum FuelTypes {None, Diesel, Benzin, Kerosin, Elektro}
     }
 }
